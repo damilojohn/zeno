75fc9008e23ba3237d76835a130ae1ec77081d2f
Damilola-John <Oduguwadamilola40@gmail.com>
2023-09-27 03:57:58 +0100

    added lambda function

--- src/app.py
+++ src/app.py
@@ -1,11 +1,45 @@
 import pinecone 
-import os 
-import pandas as pd 
-import logging 
+import os
+import logging
+import json
+from sentence_transformers import SentenceTransformer
+
+encoder = SentenceTransformer(os.getenv('ENCODER_NAME'))
 
 logger = logging.getlogger()
 logger.setLevel(logging.info)
+logger.info('connecting to vectordb')
+pinecone.init(
+    api_key=os.getenv('PINECONE_API_KEY'),
+    environment=os.getenv('PINECONE_REGION')
+)
+index_name = 'zeno'
+index = pinecone.Index(index_name)
+logger.info('connected to index....')
 
-def lambda_handler(event,_context):
-    query = event['query']
-    
+
+def query_db(index, query):
+    query_vec = encoder.encode(query).tolist()
+    matches = index.query(query, top_k=10, include_metadata=True)
+    results = []
+    for result in matches['matches']:
+        results.append(result)
+    return results
+
+
+def lambda_handler(event, _context):
+    """ Query vector db and return matches"""
+    try:
+        query = event['body']['query']
+    except TypeError:
+        query = json.loads(event['body'])
+    if query is None:
+        return {'statusCode': 400, 'message': 'no input query was provided'}
+    results = query_db(index=index, query=query)
+    return {
+        'body': json.dumps(
+            {'books': results}),
+        'statusCode': 200,
+        'headers':
+            {'content-type': 'application/json'}
+    }
