3f80f85ae81ca45daa47e2ec38dde0f32afe9d10
Damilola-John <Oduguwadamilola40@gmail.com>
2023-09-27 05:04:36 +0100

    modified dockerfile

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ logger.setLevel(logging.info)
 logger.info('connecting to vectordb')
 pinecone.init(
     api_key=os.getenv('PINECONE_API_KEY'),
-    environment=os.getenv('PINECONE_REGION')
+    environment=os.getenv('PINECONE_ENV')
 )
 index_name = 'zeno'
 index = pinecone.Index(index_name)
@@ -21,7 +21,7 @@ logger.info('connected to index....')
 def query_db(index, query):
     logger.info('generating query embeddings....')
     query_vec = encoder.encode(query).tolist()
-    logger.info('performing semantic search')
+    logger.info('performing semantic search...')
     matches = index.query(query_vec, top_k=10, include_metadata=True)
     results = []
     for result in matches['matches']:
@@ -34,7 +34,8 @@ def lambda_handler(event, _context):
     try:
         query = event['body']['query']
     except TypeError:
-        query = json.loads(event['body'])
+        request = json.loads(event['body'])
+        query - request['query']
     if query is None:
         return {'statusCode': 400, 'message': 'no input query was provided'}
     results = query_db(index=index, query=query)
