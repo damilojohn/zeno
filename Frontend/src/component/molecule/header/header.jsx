2f8389d4578f4bdcb66c971a0f622305515485b5
mankinde23 <makinded12@gmail.com>
2023-10-05 17:39:23 +0100

    changed the folder name from book search to frontend

---
+++ Frontend/src/component/molecule/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../header/header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>BOOK RECOMMENDATIONS</h1>
+
+    </div>
+  )
+}
+
+export default Header
