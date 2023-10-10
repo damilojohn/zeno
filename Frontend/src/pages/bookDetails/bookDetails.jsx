69d7db0e55994a2dd2530f8bf2612128c0ef8885
mankinde23 <makinded12@gmail.com>
2023-10-10 09:35:58 +0100

    on this page i fetched the api using axios and passing the unique identifer for each book in the api url and used useParams to grab specific book relating to the identifier

--- Frontend/src/pages/bookDetails/bookDetails.jsx
+++ Frontend/src/pages/bookDetails/bookDetails.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 const BookDetails = () => {
+  const [bookDetails, setBookDetails] = useState(null)
+  const { isbn10 } = useParams()
+  console.log(isbn10)
+  const fetchBook = async () => {
+    try {
+      const details = await axios.post(`https://hfex3k70e0.execute-api.us-east-1.amazonaws.com/prod/${isbn10}`, postData)
+      setBookDetails(details.bookDetails)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => { fetchBook() }, [])
   return (
-    <div>
-      
+    <div className='container-details'>
+
     </div>
   )
 }
