9d186584a2baad1b3c4956eb17edd8f96623328c
mankinde23 <makinded12@gmail.com>
2023-12-31 22:06:50 +0100

    started the navbar by getting the png file in the navbar component. This is a pre work before the main styling

--- Frontend/src/component/molecule/navbar/navbar.jsx
+++ Frontend/src/component/molecule/navbar/navbar.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import './navbar.css'
-import image from '/src/assets/zeno..png'
+// import image from '/src/assets/zeno..png'
+import image from '/src/assets/Vector.png'
+import image1 from '/src/assets/Vector (1).png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <ul>
+      {/* <ul>
         <li>About Us</li>
-        {/* <li className='logo'>zeno</li> */}
+       
         <img src={image} alt="logo" />
         <li>Use Cases</li>
+      </ul> */}
+      {/* /////// old prototype  */}
+     
+     <div className='navbar-left'><img src={image} alt="zeeno" /></div>
+     <div className='center '>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
       </ul>
+     </div>
+     <div className='navbar-right'><img src={image1} alt="" /></div>
+
+
+
 
     </div>
   )
