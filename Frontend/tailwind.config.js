f087ebf5313184b513b39d1a5fbfa7cd6de713b6
mankinde23 <makinded12@gmail.com>
2024-01-02 07:30:15 +0100

    installed tailwind configuration set up

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'zenno-orange':'rgba(226, 94, 62, 1)',
+      'zenno-white':'rgba(255, 255, 255, 1)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
