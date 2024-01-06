34be073c137cf81c1528e11c167b29abb8af3012
mankinde23 <makinded12@gmail.com>
2024-01-06 09:19:06 +0100

    styled the header to perfection making sure i implement the design well

--- Frontend/src/component/organism/header/header.jsx
+++ Frontend/src/component/organism/header/header.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import img from '/src/assets/Isometric Stickers - Books 1.png'
-import './header.css'
-
+import React from 'react';
+import img from '/src/assets/Isometric Stickers - Books 1.png';
+// import './header.css'
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <img src={img} alt="book" />
+  return (
+    // <div className='header'>
+    //     <img src={img} alt="book" />
 
-        </div>
-    )
-}
+    // </div>
+    <div className="flex items-center justify-center">
+      <h1 className="text-zenno-black flex items-center justify-center text-6xl font-bold  w-[47.375rem]  h-[8.3125rem] text-center text-[3.75rem]  leading-[4.00875rem] mt-space1000">
+        Discover books that <br />
+        match your imagination.
+      </h1>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
