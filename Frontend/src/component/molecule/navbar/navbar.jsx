e8fa0b3c5cc2153c13e1ca3ec6aefb736f46b798
mankinde23 <makinded12@gmail.com>
2024-01-06 09:21:38 +0100

    styled the navabr

--- Frontend/src/component/molecule/navbar/navbar.jsx
+++ Frontend/src/component/molecule/navbar/navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 // import './navbar.css'
 // import image from '/src/assets/zeno..png'
-import image from '/src/assets/Vector.png'
-import image1 from '/src/assets/Vector (1).png'
+import image from '/src/assets/Vector.png';
+import image1 from '/src/assets/Vector (1).png';
 
 const Navbar = () => {
   return (
@@ -25,27 +25,29 @@ const Navbar = () => {
     //  </div>
     //  <div className='navbar-right'><img src={image1} alt="" /></div>
 
-
-
-
     // </div>
-    <div className="flex  bg-zenno-white justify-between px-10 py-6 w-auto shadow-lg items-baseline">
-      <div className='text-zenno-black w-84'><img src={image} alt="zeeno" className='image-style-1'/></div>
-      <div className='center '>
-      
-        <ul className="flex  gap-8">
-          <li className="text-zenno-black text-center text-base hover:underline w-51 h-17">Home</li>
-          <li className="text-zenno-black text-center text-base hover:underline w-75 h-17">About Us</li>
-          <li className="text-zenno-black text-center text-base hover:underline  h-17">Contact</li>
+    <div className="flex  bg-zenno-white justify-between px-[3.12rem] py-[2.31rem] w-auto shadow-lg items-baseline">
+      <div className="text-zenno-black w-84">
+        <img src={image} alt="zeeno" className="image-style-1" />
+      </div>
+      <div className="center ">
+        <ul className="flex gap-space400">
+          <li className="text-zenno-black text-center text-base hover:underline w-51 h-17">
+            Home
+          </li>
+          <li className="text-zenno-black text-center text-base hover:underline w-75 h-17">
+            About Us
+          </li>
+          <li className="text-zenno-black text-center text-base hover:underline  h-17">
+            Contact
+          </li>
         </ul>
       </div>
-      <div className='search' >
-        <img src={image1} alt="" className='flex w-18 h-18 item-center'/>
+      <div className="search">
+        <img src={image1} alt="" className="flex w-18 h-18 item-center" />
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
