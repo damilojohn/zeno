b8bf22d3dc84ae835218a7b43516ad22428fd3a2
mankinde23 <makinded12@gmail.com>
2024-01-09 12:52:35 +0100

    added a new customized color to the application

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -15,6 +15,7 @@ export default {
         'zenno-1white': ' #FFF',
         'zenno-clear': ' rgba(255, 255, 255, 0.70)',
         'zenno-3black': '#000',
+        'zenno-4black': 'rgba(0, 0, 0, 0.80)',
       },
       fontSize: {},
       fontFamily: {},
